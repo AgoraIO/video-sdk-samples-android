f9efb56ae92e3e1c28ccb5ff2ce9598ec9409a02
saudsami <saudsami@gmail.com>
2023-09-13 16:19:08 +0500

    code cleanup

--- android-reference-app/app/src/main/java/io/agora/android_reference_app/PlayMediaActivity.kt
+++ android-reference-app/app/src/main/java/io/agora/android_reference_app/PlayMediaActivity.kt
@@ -1,9 +1,11 @@
 package io.agora.android_reference_app
 
 import android.os.Bundle
+import android.view.SurfaceView
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+
 import io.agora.mediaplayer.Constants
 import io.agora.play_media_manager.PlayMediaManager
 
@@ -11,66 +13,10 @@ class PlayMediaActivity : BasicImplementationActivity() {
     private lateinit var playMediaManager: PlayMediaManager
     private var mediaButton: Button? = null
     private var mediaProgressBar: ProgressBar? = null
-    private var isMediaFileOpened = false
 
     // In a real world app, you declare the media location variable with an empty string
     // and update it when a user chooses a media file from a local or remote source.
     private val mediaLocation = "https://www.appsloveworld.com/wp-content/uploads/2018/10/640.mp4"
-
-    //private val baseListener = agoraManagerListener
-
-    private val mediaPlayerListener: PlayMediaManager.MediaPlayerListener
-        get() = object : PlayMediaManager.MediaPlayerListener {
-            override fun onPlayerStateChanged(
-                state: Constants.MediaPlayerState,
-                error: Constants.MediaPlayerError
-            ) {
-                when (state) {
-                    Constants.MediaPlayerState.PLAYER_STATE_OPEN_COMPLETED -> {
-                        isMediaFileOpened = true
-                        runOnUiThread {
-                            mediaButton?.setText(R.string.play_media_file)
-                            mediaButton?.isEnabled = true
-                            mediaProgressBar?.progress = 0
-                        }
-                    }
-                    Constants.MediaPlayerState.PLAYER_STATE_PLAYBACK_ALL_LOOPS_COMPLETED -> {
-                        isMediaFileOpened = false
-                        playMediaManager.destroyMediaPlayer()
-                        runOnUiThread {
-                            // Restore local video
-                            val videoFrame = videoFrameMap[agoraManager.localUid]
-                            videoFrame?.removeAllViews()
-                            videoFrame?.addView(playMediaManager.localVideo)
-                            mediaButton?.setText(R.string.open_media_file)
-                            mediaButton?.isEnabled = true
-                            mediaProgressBar?.progress = 0
-                        }
-                    }
-                    Constants.MediaPlayerState.PLAYER_STATE_FAILED -> {
-                        runOnUiThread {
-                            mediaButton?.setText(R.string.open_media_file)
-                            mediaButton?.isEnabled = true
-                            mediaProgressBar?.progress = 0
-                        }
-                    }
-                    Constants.MediaPlayerState.PLAYER_STATE_PLAYING -> {
-                        runOnUiThread {
-                            mediaButton?.setText(R.string.pause)
-                            mediaButton?.isEnabled = true
-                        }
-                    }
-                    else -> {
-
-                    }
-                }
-            }
-
-            override fun onProgress(percent: Int) {
-                    // Update the ProgressBar
-                    mediaProgressBar?.progress = percent
-            }
-        }
 
     // Override the UI layout
     override val layoutResourceId: Int
@@ -82,6 +28,7 @@ class PlayMediaActivity : BasicImplementationActivity() {
         // Set up access to the UI elements
         mediaProgressBar = findViewById(R.id.MediaProgress)
         mediaButton = findViewById(R.id.MediaPlayerButton)
+        mediaButton?.isEnabled = false
     }
 
     override fun initializeAgoraManager() {
@@ -93,29 +40,31 @@ class PlayMediaActivity : BasicImplementationActivity() {
         agoraManager.setListener(agoraManagerListener)
     }
 
-
     override fun join() {
         playMediaManager.joinChannelWithToken()
+        mediaButton?.isEnabled = true
     }
 
     fun playMedia(view: View) {
         when (playMediaManager.mediaPlayerState()) {
-                Constants.MediaPlayerState.PLAYER_STATE_PLAYING -> {
-                playMediaManager.pauseMedia()
-                mediaButton?.setText(R.string.resume)
-            }
-                Constants.MediaPlayerState.PLAYER_STATE_PAUSED -> {
-                playMediaManager.resumeMedia()
-                mediaButton?.setText(R.string.pause)
-            }
-                Constants.MediaPlayerState.PLAYER_STATE_OPEN_COMPLETED -> {
-                val videoFrame = videoFrameMap[agoraManager.localUid]
-                videoFrame?.removeAllViews()
-                videoFrame?.addView(playMediaManager.mediaPlayerSurfaceView())
+            Constants.MediaPlayerState.PLAYER_STATE_OPEN_COMPLETED -> {
+                // Play
+                displayVideoInFrame(playMediaManager.mediaPlayerSurfaceView())
                 playMediaManager.playMedia()
                 mediaButton?.setText(R.string.pause)
             }
+            Constants.MediaPlayerState.PLAYER_STATE_PLAYING -> {
+                // Pause
+                playMediaManager.pauseMedia()
+                mediaButton?.setText(R.string.resume)
+            }
+            Constants.MediaPlayerState.PLAYER_STATE_PAUSED -> {
+                // Resume
+                playMediaManager.resumeMedia()
+                mediaButton?.setText(R.string.pause)
+            }
             else -> {
+                // Open media file
                 playMediaManager.setupMediaPlayer(mediaPlayerListener)
                 playMediaManager.openMediaFile(mediaLocation)
                 mediaButton?.isEnabled = false
@@ -123,4 +72,86 @@ class PlayMediaActivity : BasicImplementationActivity() {
             }
         }
     }
+
+    private val mediaPlayerListener: PlayMediaManager.MediaPlayerListener
+        get() = object : PlayMediaManager.MediaPlayerListener {
+            override fun onPlayerStateChanged(
+                state: Constants.MediaPlayerState,
+                error: Constants.MediaPlayerError
+            ) {
+                if (error != Constants.MediaPlayerError.PLAYER_ERROR_NONE) {
+                    showMessage(error.name)
+                }
+
+                when (state) {
+                    Constants.MediaPlayerState.PLAYER_STATE_OPEN_COMPLETED -> {
+                        runOnUiThread {
+                            // Ready to play, update button state
+                            mediaButton?.setText(R.string.play_media_file)
+                            mediaButton?.isEnabled = true
+                            mediaProgressBar?.progress = 0
+                        }
+                    }
+                    Constants.MediaPlayerState.PLAYER_STATE_PLAYBACK_ALL_LOOPS_COMPLETED -> {
+                        // Media finished playing
+                        playMediaManager.destroyMediaPlayer()
+                        runOnUiThread {
+                            // Restore local video
+                            displayVideoInFrame(playMediaManager.localVideo)
+                            playMediaManager.updateChannelPublishOptions(false)
+                            // Update the button caption and state
+                            mediaButton?.setText(R.string.open_media_file)
+                            mediaButton?.isEnabled = true
+                            mediaProgressBar?.progress = 0
+                        }
+                    }
+                    Constants.MediaPlayerState.PLAYER_STATE_FAILED -> {
+                        runOnUiThread {
+                            // Failed to open media file
+                            showMessage("Failed to open media file.")
+                            // Update the button caption and state
+                            mediaButton?.setText(R.string.open_media_file)
+                            mediaButton?.isEnabled = true
+                            mediaProgressBar?.progress = 0
+                        }
+                    }
+                    Constants.MediaPlayerState.PLAYER_STATE_PLAYING -> {
+                        runOnUiThread {
+                            // Update the button caption and state
+                            mediaButton?.setText(R.string.pause)
+                            mediaButton?.isEnabled = true
+                        }
+                    }
+                    else -> {
+
+                    }
+                }
+            }
+
+            override fun onProgress(percent: Int) {
+                // Update the ProgressBar
+                mediaProgressBar?.progress = percent
+            }
+        }
+
+    private fun displayVideoInFrame(surfaceView: SurfaceView) {
+        // Add the SurfaceView to a FrameLayout
+        val videoFrame = videoFrameMap[agoraManager.localUid]
+        videoFrame?.removeAllViews()
+        videoFrame?.addView(surfaceView)
+        if (videoFrame == mainFrame) {
+            surfaceViewMain = surfaceView
+        }
+    }
+
+    override fun leave() {
+        // Release media player resources
+        playMediaManager.destroyMediaPlayer()
+        // Update the button caption and state
+        mediaButton?.isEnabled = false
+        mediaButton?.setText(R.string.open_media_file)
+        mediaProgressBar?.progress = 0
+        super.leave()
+    }
+
 }
--- play-media/src/main/java/io/agora/play_media_manager/PlayMediaManager.kt
+++ play-media/src/main/java/io/agora/play_media_manager/PlayMediaManager.kt
@@ -14,9 +14,14 @@ import io.agora.rtc2.video.VideoCanvas
 
 class PlayMediaManager(context: Context?) : AuthenticationManager(context) {
     private var mediaPlayer: IMediaPlayer? = null // Instance of the media player
-    var isMediaPlaying = false
     private var mediaDuration: Long = 0
     private lateinit var mediaPlayerListener: MediaPlayerListener
+
+    // Interface to forward media player events to the UI
+    interface MediaPlayerListener {
+        fun onPlayerStateChanged(state: MediaPlayerState, error: MediaPlayerError)
+        fun onProgress(percent: Int)
+    }
 
     fun setupMediaPlayer(listener: MediaPlayerListener){
         if (mediaPlayer == null) {
@@ -24,19 +29,19 @@ class PlayMediaManager(context: Context?) : AuthenticationManager(context) {
             mediaPlayer = agoraEngine?.createMediaPlayer()
             // Set the mediaPlayerObserver to receive callbacks
             mediaPlayer?.registerPlayerObserver(mediaPlayerObserver)
-
+            // A listener to notify the UI
             mediaPlayerListener = listener
-            sendMessage("Opening media file...")
-            return
         }
     }
 
     fun mediaPlayerState(): MediaPlayerState? {
-        // Check the current state of the media player
+        // Returns the current state of the media player
         return mediaPlayer?.state
     }
 
     fun openMediaFile(mediaLocation: String) {
+        // Opens the media file at mediaLocation url
+        // Supports URI files starting with content://
         if (mediaPlayer != null) {
             // Open the media file
             mediaPlayer?.open(mediaLocation, 0)
@@ -44,7 +49,7 @@ class PlayMediaManager(context: Context?) : AuthenticationManager(context) {
     }
 
     fun destroyMediaPlayer(){
-        // Destroy and clean up
+        // Destroy the media player instance and clean up
         if (mediaPlayer != null) {
             mediaPlayer?.stop()
             mediaPlayer?.unRegisterPlayerObserver(mediaPlayerObserver)
@@ -54,6 +59,7 @@ class PlayMediaManager(context: Context?) : AuthenticationManager(context) {
     }
 
     fun playMedia() {
+        // Start publishing the media player video
         updateChannelPublishOptions(true)
         mediaPlayer?.play()
     }
@@ -66,92 +72,91 @@ class PlayMediaManager(context: Context?) : AuthenticationManager(context) {
         mediaPlayer?.resume()
     }
 
-    private fun updateChannelPublishOptions(publishMediaPlayer: Boolean) {
+    fun updateChannelPublishOptions(publishMediaPlayer: Boolean) {
         val channelOptions = ChannelMediaOptions()
+        // Start or stop publishing the media player tracks
         channelOptions.publishMediaPlayerAudioTrack = publishMediaPlayer
         channelOptions.publishMediaPlayerVideoTrack = publishMediaPlayer
+        // Stop or start publishing the microphone and camera tracks
         channelOptions.publishMicrophoneTrack = !publishMediaPlayer
         channelOptions.publishCameraTrack = !publishMediaPlayer
+        // Specify the media player Id for publishing
         if (publishMediaPlayer) channelOptions.publishMediaPlayerId = mediaPlayer?.mediaPlayerId
+        // Implement the settings
         agoraEngine?.updateChannelMediaOptions(channelOptions)
     }
 
     fun mediaPlayerSurfaceView(): SurfaceView {
+        // Sets up and returns a SurfaceView to display the media player output
+        // Instantiate a SurfaceView
         val videoSurfaceView = SurfaceView(mContext)
-        // Setup the SurfaceView to display media player output
+        // Create a VideoCanvas using the SurfaceView
         val videoCanvas =  VideoCanvas(
             videoSurfaceView,
             Constants.RENDER_MODE_HIDDEN,
             0
         )
-        videoCanvas.mediaPlayerId = mediaPlayer?.mediaPlayerId ?: 0
+        // Set the source type and media player Id
         videoCanvas.sourceType = Constants.VIDEO_SOURCE_MEDIA_PLAYER
+        videoCanvas.mediaPlayerId = mediaPlayer?.mediaPlayerId ?: 0
+        // Setup the SurfaceView
         agoraEngine?.setupLocalVideo(videoCanvas)
+
         return videoSurfaceView
     }
 
     private val mediaPlayerObserver: IMediaPlayerObserver = object : IMediaPlayerObserver {
         override fun onPlayerStateChanged(state: MediaPlayerState, error: MediaPlayerError) {
+            // Reports the changes of playback state
             // Notify the UI
             mediaPlayerListener.onPlayerStateChanged(state, error)
 
             if (state == MediaPlayerState.PLAYER_STATE_OPEN_COMPLETED) {
-                // Media file opened successfully
+                // Read media duration for updating play progress
                 mediaDuration = mediaPlayer?.duration ?: 0
-            } else if (state == MediaPlayerState.PLAYER_STATE_PLAYBACK_ALL_LOOPS_COMPLETED) {
-                isMediaPlaying = false
-                // Stop publishing media player output and restore local video publishing
-                updateChannelPublishOptions(false)
             }
         }
 
         override fun onPositionChanged(position: Long) {
             if (mediaDuration > 0) {
+                // Calculate the progress percentage
                 val result = (position.toFloat() / mediaDuration.toFloat() * 100).toInt()
+                // Notify the UI of the progress
                 mediaPlayerListener.onProgress(result)
             }
         }
 
-        override fun onPlayerEvent(
-            eventCode: MediaPlayerEvent,
-            elapsedTime: Long,
-            message: String
-        ) {
+        override fun onPlayerEvent(eventCode: MediaPlayerEvent, elapsedTime: Long, message: String) {
             // Required to implement IMediaPlayerObserver
         }
 
         override fun onMetaData(type: MediaPlayerMetadataType, data: ByteArray) {
-            // Required to implement IMediaPlayerObserver
+            // Occurs when the media metadata is received
         }
 
         override fun onPlayBufferUpdated(playCachedBuffer: Long) {
-            // Required to implement IMediaPlayerObserver
+            // Reports the playback duration that the buffered data can support
         }
 
         override fun onPreloadEvent(src: String, event: MediaPlayerPreloadEvent) {
-            // Required to implement IMediaPlayerObserver
+            // Reports the events of preloaded media resources
+        }
+
+        override fun onPlayerSrcInfoChanged(from: SrcInfo, to: SrcInfo) {
+            // Occurs when the video bitrate of the media resource changes
+        }
+
+        override fun onPlayerInfoUpdated(info: PlayerUpdatedInfo) {
+            // Occurs when information related to the media player changes
+        }
+
+        override fun onAudioVolumeIndication(volume: Int) {
+            // Reports the volume of the media player
         }
 
         override fun onAgoraCDNTokenWillExpire() {
             // Required to implement IMediaPlayerObserver
         }
-
-        override fun onPlayerSrcInfoChanged(from: SrcInfo, to: SrcInfo) {
-            // Required to implement IMediaPlayerObserver
-        }
-
-        override fun onPlayerInfoUpdated(info: PlayerUpdatedInfo) {
-            // Required to implement IMediaPlayerObserver
-        }
-
-        override fun onAudioVolumeIndication(volume: Int) {
-            // Required to implement IMediaPlayerObserver
-        }
     }
 
-    // Interface to forward media player events to the UI
-    interface MediaPlayerListener {
-        fun onPlayerStateChanged(state: MediaPlayerState, error: MediaPlayerError)
-        fun onProgress(percent: Int)
-    }
 }
