f2269f7a7bcf6187cfc155667127be3901187a5a
Saud <65331551+saudsami@users.noreply.github.com>
2023-11-03 09:37:30 +0500

    Volume Updates

--- android-reference-app/app/src/main/java/io/agora/android_reference_app/ProductWorkflowActivity.kt
+++ android-reference-app/app/src/main/java/io/agora/android_reference_app/ProductWorkflowActivity.kt
@@ -3,16 +3,14 @@ package io.agora.android_reference_app
 import android.os.Bundle
 import android.view.SurfaceView
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
 
 import io.agora.mediaplayer.Constants
 import io.agora.product_workflow_manager.ProductWorkflowManager
 
 class ProductWorkflowActivity : BasicImplementationActivity() {
     private lateinit var productWorkflowManager: ProductWorkflowManager
+    var selectedVolumeType = ProductWorkflowManager.VolumeTypes.PLAYBACK_SIGNAL_VOLUME
 
     // Override the UI layout
     override val layoutResourceId: Int
@@ -21,39 +19,53 @@ class ProductWorkflowActivity : BasicImplementationActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Set up access to the UI elements
-        setUpVolumeSpinner()
+        // Set up the UI elements
+        setupVolumeSpinner()
+        setupVolumeSeekbar()
     }
 
-    private fun setUpVolumeSpinner() {
+    private fun setupVolumeSeekbar() {
+        val seekBar = findViewById<SeekBar>(R.id.volumeSeekBar)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // Handle the SeekBar value change here
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // This method is called when the user starts dragging the SeekBar
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // This method is called when the user stops dragging the SeekBar
+                productWorkflowManager.adjustVolume(selectedVolumeType, seekBar.progress)
+                showMessage("Setting ${selectedVolumeType.description} to ${seekBar.progress}")
+            }
+        })
+    }
+
+    private fun setupVolumeSpinner() {
         val volumeSpinner: Spinner = findViewById(R.id.volumeSpinner)
 
-        // Sample data for the spinner
-        val options = listOf(
-            "Recording signal volume",
-            "Playback signal volume",
-            "User playback signal volume",
-            "Audio mixing volume",
-            "Audio mixing play-out volume",
-            "Audio mixing publish volume",
-            "In-ear monitoring volume"
-            )
+        // Create an array of enum values
+        val volumeTypes = ProductWorkflowManager.VolumeTypes.values()
 
-        // Create an ArrayAdapter
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        // Create an array of strings for display
+        val volumeTypeDescriptions = volumeTypes.map { it.description }.toTypedArray()
+
+        // Create an adapter to populate the Spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, volumeTypeDescriptions)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         volumeSpinner.adapter = adapter
 
-        // Set a listener for spinner item selection
         volumeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedOption = options[position]
-                // Execute code based on the selected option
-                showMessage(selectedOption)
+                selectedVolumeType = volumeTypes[position]
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                // Do nothing
+                // Handle nothing selected
             }
         }
     }
--- product-workflow/src/main/java/io/agora/product_workflow_manager/ProductWorkflowManager.kt
+++ product-workflow/src/main/java/io/agora/product_workflow_manager/ProductWorkflowManager.kt
@@ -10,6 +10,51 @@ import io.agora.rtc2.ScreenCaptureParameters
 
 class ProductWorkflowManager(context: Context?) : AuthenticationManager(context) {
 
+    public enum class VolumeTypes(val description: String, val intValue: Int) {
+        PLAYBACK_SIGNAL_VOLUME("Playback signal volume", 1),
+        RECORDING_SIGNAL_VOLUME("Recording signal volume", 2),
+        USER_PLAYBACK_SIGNAL_VOLUME("User playback signal volume", 3),
+        AUDIO_MIXING_VOLUME("Audio mixing volume", 4),
+        AUDIO_MIXING_PLAYOUT_VOLUME("Audio mixing play-out volume", 5),
+        AUDIO_MIXING_PUBLISH_VOLUME("Audio mixing publish volume", 6),
+        CUSTOM_AUDIO_PLAYOUT_VOLUME("Custom audio play-out volume", 7),
+        CUSTOM_AUDIO_PUBLISH_VOLUME("Custom audio publish volume", 8),
+    }
+
+    fun adjustVolume(volumeParameter: VolumeTypes, volume: Int) {
+        when (volumeParameter) {
+            VolumeTypes.PLAYBACK_SIGNAL_VOLUME -> {
+                agoraEngine?.adjustPlaybackSignalVolume(volume)
+            }
+            VolumeTypes.RECORDING_SIGNAL_VOLUME -> {
+                agoraEngine?.adjustRecordingSignalVolume(volume)
+            }
+            VolumeTypes.USER_PLAYBACK_SIGNAL_VOLUME -> {
+                if (remoteUids.size > 0) {
+                    val remoteUid = remoteUids.first() // the uid of the remote user
+                    agoraEngine?.adjustUserPlaybackSignalVolume(remoteUid, volume)
+                }
+            }
+            VolumeTypes.AUDIO_MIXING_VOLUME -> {
+                agoraEngine?.adjustAudioMixingVolume(volume)
+            }
+            VolumeTypes.AUDIO_MIXING_PLAYOUT_VOLUME -> {
+                agoraEngine?.adjustAudioMixingPlayoutVolume(volume)
+            }
+            VolumeTypes.AUDIO_MIXING_PUBLISH_VOLUME -> {
+                agoraEngine?.adjustAudioMixingPublishVolume(volume)
+            }
+            VolumeTypes.CUSTOM_AUDIO_PLAYOUT_VOLUME -> {
+                agoraEngine?.adjustAudioMixingPlayoutVolume(volume)
+            }
+            VolumeTypes.CUSTOM_AUDIO_PUBLISH_VOLUME -> {
+                val trackId = 0 // use the id of your custom audio track
+                agoraEngine?.adjustCustomAudioPublishVolume(trackId, volume)
+            }
+            else -> {}
+        }
+    }
+
     fun updateChannelPublishOptions(publishMediaPlayer: Boolean) {
         val channelOptions = ChannelMediaOptions()
         // Start or stop publishing the media player tracks
