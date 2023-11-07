4eafd4bdfca9102b53cbc0faf136aff07306cc77
saudsami <saudsami@gmail.com>
2023-11-07 16:53:22 +0500

    Updates

--- android-reference-app/app/src/main/java/io/agora/android_reference_app/AudioVoiceEffectsActivity.kt
+++ android-reference-app/app/src/main/java/io/agora/android_reference_app/AudioVoiceEffectsActivity.kt
@@ -1,14 +1,38 @@
 package io.agora.android_reference_app
 
+import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.appcompat.widget.SwitchCompat
 import io.agora.audio_voice_effects_manager.AudioVoiceEffectsManager
 
 class AudioVoiceEffectsActivity : BasicImplementationActivity() {
     private lateinit var audioVoiceEffectsManager: AudioVoiceEffectsManager
 
+    private val soundEffectId = 1 // Unique identifier for the sound effect file
+    private val soundEffectFilePath =
+        "https://www.soundjay.com/human/applause-01.mp3" // URL or path to the sound effect
+    private var soundEffectStatus = 0
+    private var voiceEffectIndex = 0
+    private var audioPlaying = false // Manage the audio mixing state
+    private val audioFilePath =
+        "https://www.kozco.com/tech/organfinale.mp3" // URL or path to the audio mixing file
+    private var playEffectButton: Button? = null
+    private var voiceEffectButton:Button? = null
+    private var speakerphoneSwitch: SwitchCompat? = null
+
     // Override the UI layout
     override val layoutResourceId: Int
         get() = R.layout.activity_audio_voice_effects
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        speakerphoneSwitch = findViewById(R.id.SwitchSpeakerphone)
+        speakerphoneSwitch!!.setOnCheckedChangeListener { buttonView, isChecked ->
+
+        }
+    }
 
     override fun initializeAgoraManager() {
         // Instantiate an object of the PlayMediaManager
@@ -23,7 +47,42 @@ class AudioVoiceEffectsActivity : BasicImplementationActivity() {
         audioVoiceEffectsManager.joinChannelWithToken()
     }
 
-    fun audioMixing(view: View) {}
-    fun playSoundEffect(view: View) {}
-    fun applyVoiceEffect(view: View) {}
+    fun audioMixing(view: View) {
+        val button: Button =  findViewById(R.id.AudioMixingButton)
+        audioPlaying = !audioPlaying
+
+        if (audioPlaying) {
+            button.setText(getString(R.string.stop_audio_mixing))
+            try {
+                audioVoiceEffectsManager. startMixing(audioFilePath, false, 1, 0)
+                showMessage("Audio playing")
+            } catch (e: Exception) {
+                showMessage("Exception playing audio\n$e")
+            }
+        } else {
+            audioVoiceEffectsManager.stopMixing()
+            button.setText(R.string.start_audio_mixing)
+        }
+    }
+
+    fun playSoundEffect(view: View) {
+        if (playEffectButton == null) playEffectButton = view as Button
+        if (soundEffectStatus == 0) { // Stopped
+            audioVoiceEffectsManager.playEffect(soundEffectId, soundEffectFilePath)
+            playEffectButton?.text = getString(R.string.pause_audio_effect)
+            soundEffectStatus = 1
+        } else if (soundEffectStatus == 1) { // Playing
+            audioVoiceEffectsManager.pauseEffect(soundEffectId)
+            soundEffectStatus = 2
+            playEffectButton?.text = getString(R.string.resume_audio_effect)
+        } else if (soundEffectStatus == 2) { // Paused
+            audioVoiceEffectsManager.resumeEffect(soundEffectId)
+            soundEffectStatus = 1
+            playEffectButton?.text  =getString(R.string.pause_audio_effect)
+        }
+    }
+
+    fun applyVoiceEffect(view: View) {
+
+    }
 }
--- audio-and-voice-effects/src/main/java/io/agora/audio_voice_effects_manager/AudioVoiceEffectsManager.kt
+++ audio-and-voice-effects/src/main/java/io/agora/audio_voice_effects_manager/AudioVoiceEffectsManager.kt
@@ -1,12 +1,85 @@
 package io.agora.audio_voice_effects_manager
 
 import android.content.Context
+import androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread
 import io.agora.authentication_manager.AuthenticationManager
-import io.agora.rtc2.RtcEngine
-import io.agora.rtc2.RtcEngineConfig
-import java.lang.Exception
+import io.agora.rtc2.IAudioEffectManager
+import io.agora.rtc2.IRtcEngineEventHandler
+
 
 class AudioVoiceEffectsManager(context: Context?) : AuthenticationManager(context) {
+    private var audioEffectManager: IAudioEffectManager? = null
+    private val baseEventHandler: IRtcEngineEventHandler? // To extend the event handler from the base class
 
-   
+    init {
+        baseEventHandler = super.iRtcEngineEventHandler
+    }
+
+    fun startMixing(audioFilePath: String, loopBack: Boolean, cycle: Int, startPos: Int) {
+        agoraEngine?.startAudioMixing(audioFilePath, loopBack, cycle, startPos)
+    }
+
+    fun stopMixing() {
+        agoraEngine?.stopAudioMixing()
+    }
+
+    fun playEffect(soundEffectId: Int, soundEffectFilePath: String) {
+        if (audioEffectManager == null) {
+            // Set up the audio effects manager
+            audioEffectManager = agoraEngine?.audioEffectManager
+            // Pre-load sound effects to improve performance
+            audioEffectManager?.preloadEffect(soundEffectId, soundEffectFilePath)
+        }
+
+        audioEffectManager!!.playEffect(
+            soundEffectId,  // The ID of the sound effect file.
+            soundEffectFilePath,  // The path of the sound effect file.
+            0, 1.0,  // The pitch of the audio effect. 1 represents the original pitch.
+            0.0, 100.0,  // The volume of the audio effect. 100 represents the original volume.
+            true,  // Whether to publish the audio effect to remote users.
+            0 // The playback starting position of the audio effect file in ms.
+        )
+    }
+
+    fun pauseEffect(soundEffectId: Int) {
+        audioEffectManager!!.pauseEffect(soundEffectId)
+    }
+
+    fun resumeEffect(soundEffectId: Int) {
+        audioEffectManager!!.resumeEffect(soundEffectId)
+    }
+
+        override val iRtcEngineEventHandler: IRtcEngineEventHandler
+        get() = object : IRtcEngineEventHandler() {
+
+            // Occurs when the audio effect playback finishes.
+            override fun onAudioEffectFinished(soundId: Int) {
+                super.onAudioEffectFinished(soundId)
+                sendMessage("Audio effect finished")
+                audioEffectManager!!.stopEffect(soundId)
+                mListener.
+            }
+
+            // Listen for the event that the token is about to expire
+            override fun onTokenPrivilegeWillExpire(token: String) {
+                baseEventHandler!!.onTokenPrivilegeWillExpire(token)
+            }
+
+            // Reuse events handlers from the base class
+            override fun onUserJoined(uid: Int, elapsed: Int) {
+                baseEventHandler!!.onUserJoined(uid, elapsed)
+            }
+
+            override fun onJoinChannelSuccess(channel: String, uid: Int, elapsed: Int) {
+                baseEventHandler!!.onJoinChannelSuccess(channel, uid, elapsed)
+            }
+
+            override fun onUserOffline(uid: Int, reason: Int) {
+                baseEventHandler!!.onUserOffline(uid, reason)
+            }
+
+            override fun onConnectionStateChanged(state: Int, reason: Int) {
+                connectionStateChanged(state, reason)
+            }
+        }
 }
