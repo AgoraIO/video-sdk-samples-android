f390b24763d55911c5de26bb76e291f1aed5beb0
Saud <65331551+saudsami@users.noreply.github.com>
2023-11-12 20:57:12 +0500

    multiple channels update

--- android-reference-app/app/src/main/java/io/agora/android_reference_app/MultipleChannelsActivity.kt
+++ android-reference-app/app/src/main/java/io/agora/android_reference_app/MultipleChannelsActivity.kt
@@ -1,14 +1,17 @@
 package io.agora.android_reference_app
 
 import android.view.View
+import android.widget.Button
 import io.agora.multiple_channels_manager.MultipleChannelsManager
 
 class MultipleChannelsActivity : BasicImplementationActivity() {
     private lateinit var multipleChannelsManager: MultipleChannelsManager
+    private var channelMediaButton: Button? = null
+    private var secondChannelButton: Button? = null
 
     // Override the UI layout
     override val layoutResourceId: Int
-        get() = R.layout.activity_basic_implementation
+        get() = R.layout.activity_multiple_channels
 
     override fun initializeAgoraManager() {
         // Instantiate an object of the PlayMediaManager
@@ -17,11 +20,60 @@ class MultipleChannelsActivity : BasicImplementationActivity() {
 
         // Set up a listener for updating the UI
         agoraManager.setListener(agoraManagerListener)
+
+        channelMediaButton = findViewById(R.id.mediaRelayButton)
+        secondChannelButton = findViewById(R.id.secondChannelButton)
     }
 
     override fun join() {
         multipleChannelsManager.joinChannelWithToken()
     }
 
-    fun channelRelay(view: View) {}
+    override fun handleEngineEvent(eventName: String, eventArgs: Map<String, Any>) {
+
+        if (eventName == "onChannelMediaRelayStateChanged") {
+            when (eventArgs["state"]) {
+                // This example shows toast messages when the relay state changes,
+                // a production level app needs to handle state change properly.
+                1 -> {
+                    showMessage("Channel media relay connecting.")
+                    runOnUiThread { channelMediaButton?.text = getString(R.string.connecting_) }
+                }
+                2 -> {
+                    showMessage("Channel media relay running.")
+                    runOnUiThread {
+                        channelMediaButton?.text = getString(R.string.stop_channel_media_relay)
+                    }
+                }
+                3 -> {
+                    if (eventArgs["code"] == 2) {
+                        showMessage("No server response. Make sure that co-hosting token authentication has been enabled for your project.")
+                    } else {
+                        showMessage("Channel media relay failure. Error code: ${eventArgs["code"]}")
+                    }
+                    runOnUiThread {
+                        channelMediaButton?.text = getString(R.string.start_channel_media_relay)
+                    }
+                }
+                else -> {
+
+                }
+            }
+        } else if (eventName == "onJoinChannelSuccess2") {
+            runOnUiThread { secondChannelButton?.text = getString(R.string.leave_second_channel) }
+        } else if (eventName == "onLeaveChannel2") {
+            runOnUiThread { secondChannelButton?.text = getString(R.string.join_second_channel) }
+        } else if (eventName == "onUserJoined2") {
+            // Display remote video
+            showMessage("user joined")
+        }
+    }
+
+    fun channelRelay(view: View) {
+        multipleChannelsManager.channelRelay()
+    }
+
+    fun joinSecondChannel(view: View) {
+        multipleChannelsManager.joinSecondChannel()
+    }
 }
--- live-streaming-over-multiple-channels/src/main/java/io/agora/multiple_channels_manager/MultipleChannelsManager.kt
+++ live-streaming-over-multiple-channels/src/main/java/io/agora/multiple_channels_manager/MultipleChannelsManager.kt
@@ -1,13 +1,39 @@
 package io.agora.multiple_channels_manager
 
 import android.content.Context
+import android.view.SurfaceView
 import io.agora.authentication_manager.AuthenticationManager
-import io.agora.rtc2.RtcEngine
-import io.agora.rtc2.RtcEngineConfig
-import io.agora.rtc2.RtcEngineConfig.AreaCode.*
-import java.lang.Exception
+import io.agora.rtc2.*
+import io.agora.rtc2.video.ChannelMediaInfo
+import io.agora.rtc2.video.ChannelMediaRelayConfiguration
+import io.agora.rtc2.video.VideoCanvas
+
 
 class MultipleChannelsManager(context: Context?) : AuthenticationManager(context) {
+    private val baseEventHandler: IRtcEngineEventHandler? // To extend the event handler from the base class
+
+    // Channel media relay variables
+    private var destinationChannelName: String // Name of the destination channel
+    private var destinationChannelToken: String // Access token for the destination channel
+    private var destinationChannelUid = 0 // User ID that the user uses in the destination channel
+    private var sourceChannelToken: String // Access token for the source channel, Generate using channelName and uid = 0
+    private var mediaRelaying = false
+
+    // Multi channel streaming variables
+    private lateinit var agoraEngineEx: RtcEngineEx
+    private var rtcSecondConnection: RtcConnection? = null
+    private val secondChannelName = "demo2"// name of the second channel"
+    private val secondChannelUid = 100 // Uid for the second channel
+    private val secondChannelToken = "007eJxTYFDaYHCJ/f/xadUWvypW2aw/c9l0VsAModXvdh1z3WfXv+mqAkOquYWZoZmFsZGJuZlJUrKBBVDA3MjEIjkpzcQ4NcXw6JuA1IZARgaTkHYmRgZGBhYgBvGZwCQzmGQBk6wMKam5+UYMDABghSPM" // Access token for the second channel
+    private var isSecondChannelJoined = false // Track connection state of the second channel
+
+    init {
+        baseEventHandler = super.iRtcEngineEventHandler
+        destinationChannelName = config!!.optString("destinationChannelName")
+        destinationChannelUid = config!!.optInt("destinationChannelUid")
+        destinationChannelToken = config!!.optString("destinationChannelToken")
+        sourceChannelToken = config!!.optString("sourceChannelToken")
+    }
 
     override fun setupAgoraEngine(): Boolean {
         try {
@@ -17,10 +43,6 @@ class MultipleChannelsManager(context: Context?) : AuthenticationManager(context
             config.mAppId = appId
             // Assign an event handler to receive engine callbacks
             config.mEventHandler = iRtcEngineEventHandler
-
-            // Set the geofencing area code(s)
-            config.mAreaCode = AREA_CODE_NA or AREA_CODE_EU // AreaCodes support bitwise operations
-            sendMessage("Setting region for connection")
 
             // Create an RtcEngine instance
             agoraEngine = RtcEngine.create(config)
@@ -33,4 +55,136 @@ class MultipleChannelsManager(context: Context?) : AuthenticationManager(context
         }
         return true
     }
+
+    fun channelRelay() {
+        if (mediaRelaying) {
+            agoraEngine!!.stopChannelMediaRelay()
+        } else {
+            // Configure the source channel information.
+            val srcChannelInfo = ChannelMediaInfo(channelName, sourceChannelToken, 0)
+            val mediaRelayConfiguration = ChannelMediaRelayConfiguration()
+            mediaRelayConfiguration.setSrcChannelInfo(srcChannelInfo)
+
+            // Configure the destination channel information.
+            val destChannelInfo = ChannelMediaInfo(destinationChannelName, destinationChannelToken, destinationChannelUid)
+            mediaRelayConfiguration.setDestChannelInfo(destinationChannelName, destChannelInfo)
+
+            // Start relaying media streams across channels
+            agoraEngine!!.startOrUpdateChannelMediaRelay(mediaRelayConfiguration)
+        }
+    }
+
+    // Listen for the event that a token is about to expire
+    override val iRtcEngineEventHandler: IRtcEngineEventHandler
+        get() = object : IRtcEngineEventHandler() {
+            override fun onChannelMediaRelayStateChanged(state: Int, code: Int) {
+                if (state == 2) {
+                    mediaRelaying = true
+                } else if (state == 3) {
+                    mediaRelaying = false
+                }
+                // Inform the UI
+                val eventArgs = mutableMapOf<String, Any>()
+                eventArgs["state"] = state
+                eventArgs["code"] = code
+                mListener?.onEngineEvent("onChannelMediaRelayStateChanged", eventArgs)
+            }
+
+            // Listen for the event that the token is about to expire
+            override fun onTokenPrivilegeWillExpire(token: String) {
+                baseEventHandler!!.onTokenPrivilegeWillExpire(token)
+            }
+
+            // Reuse events handlers from the base class
+            override fun onUserJoined(uid: Int, elapsed: Int) {
+                baseEventHandler!!.onUserJoined(uid, elapsed)
+            }
+
+            override fun onJoinChannelSuccess(channel: String, uid: Int, elapsed: Int) {
+                baseEventHandler!!.onJoinChannelSuccess(channel, uid, elapsed)
+                handleOnJoinChannelSuccess(channel, uid, elapsed)
+            }
+
+            override fun onUserOffline(uid: Int, reason: Int) {
+                baseEventHandler!!.onUserOffline(uid, reason)
+            }
+
+            override fun onConnectionStateChanged(state: Int, reason: Int) {
+                connectionStateChanged(state, reason)
+            }
+        }
+
+    fun joinSecondChannel() {
+        // Create an RtcEngineEx instance
+        agoraEngineEx = RtcEngineEx.create(mContext, appId, secondChannelEventHandler) as RtcEngineEx
+        // By default, the video module is disabled, call enableVideo to enable it.
+        agoraEngineEx.enableVideo()
+
+        if (isSecondChannelJoined) {
+            agoraEngineEx.leaveChannelEx(rtcSecondConnection)
+        } else {
+            val mediaOptions = ChannelMediaOptions()
+            if (!isBroadcaster) { // Audience Role
+                mediaOptions.autoSubscribeAudio = true
+                mediaOptions.autoSubscribeVideo = true
+                mediaOptions.clientRoleType = Constants.CLIENT_ROLE_AUDIENCE
+            } else { // Host Role
+                mediaOptions.publishCameraTrack = true
+                mediaOptions.publishMicrophoneTrack = true
+                mediaOptions.channelProfile = Constants.CHANNEL_PROFILE_LIVE_BROADCASTING
+                mediaOptions.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER
+            }
+            rtcSecondConnection = RtcConnection()
+            rtcSecondConnection!!.channelId = secondChannelName
+            rtcSecondConnection!!.localUid = secondChannelUid
+            agoraEngineEx.joinChannelEx(
+                secondChannelToken,
+                rtcSecondConnection,
+                mediaOptions,
+                secondChannelEventHandler
+            )
+        }
+    }
+
+    // Callbacks for the second channel
+    private val secondChannelEventHandler: IRtcEngineEventHandler =
+        object : IRtcEngineEventHandler() {
+            override fun onJoinChannelSuccess(channel: String, uid: Int, elapsed: Int) {
+                isSecondChannelJoined = true
+                sendMessage("Joined channel $secondChannelName, uid: $uid")
+                val eventArgs = mutableMapOf<String, Any>()
+                eventArgs["channel"] = channel
+                eventArgs["uid"] = uid
+                mListener?.onEngineEvent("onJoinChannelSuccess2", eventArgs)
+            }
+
+            override fun onLeaveChannel(stats: RtcStats) {
+                isSecondChannelJoined = false
+                sendMessage("Left the channel $secondChannelName")
+                val eventArgs = mutableMapOf<String, Any>()
+                eventArgs["stats"] = stats
+                mListener?.onEngineEvent("onLeaveChannel2", eventArgs)
+            }
+
+            override fun onUserJoined(uid: Int, elapsed: Int) {
+                sendMessage(String.format("user %d joined!", uid))
+
+                // Create surfaceView for remote video
+                val remoteSurfaceView = SurfaceView(mContext)
+                remoteSurfaceView.setZOrderMediaOverlay(true)
+
+                // Setup remote video to render
+                agoraEngineEx.setupRemoteVideoEx(
+                    VideoCanvas(
+                        remoteSurfaceView,
+                        VideoCanvas.RENDER_MODE_HIDDEN, uid
+                    ), rtcSecondConnection
+                )
+
+                val eventArgs = mutableMapOf<String, Any>()
+                eventArgs["uid"] = uid
+                eventArgs["surfaceView"] = remoteSurfaceView
+                mListener?.onEngineEvent("onUserJoined2", eventArgs)
+            }
+        }
 }
